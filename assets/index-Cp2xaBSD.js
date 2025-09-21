e96c2c3a07c3dec8c136c3cc9cbf8d991f9c919c
yordalinam <yordalina.metodieva@gmail.com>
2025-09-21 14:31:22 +0300

    Updates

--- assets/index-Cp2xaBSD.js
+++ assets/index-Cp2xaBSD.js
@@ -174,14 +174,15 @@ min-height: 100vh;
     justify-content: center;
     align-items: center;
     text-align: center ;
-    padding-top: 0;
+    padding-top: 80px;
+    min-height: auto;
     } 
 `,gv=W.div`
   position: absolute;
   right: 0.5rem;
   top: 7rem;
-  height: 18rem;
-  width: 32rem;
+  height: 13rem;
+  width: 30rem;
   border-radius: 1.5rem;
   z-index: 0;
   background: linear-gradient(to right, #5f1e87, #826bc8, #c661d1);
