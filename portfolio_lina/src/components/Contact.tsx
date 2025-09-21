14268ac4bde4c0f79ee4a74dd232bff93e8cb770
yordalinam <yordalina.metodieva@gmail.com>
2025-09-21 16:15:19 +0300

    fix img + svgs

--- portfolio_lina/src/components/Contact.tsx
+++ portfolio_lina/src/components/Contact.tsx
@@ -25,8 +25,8 @@ text-align: center;
   border-top: 2px solid #b324d7;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
   gap: 1rem;
       padding: 1.5rem;
 
@@ -42,18 +42,6 @@ text-align: center;
     }
 `
 
-const ContactTitle = styled.h2`
-padding-left: 20px;
-  color: #9e22bd;
-  font-weight: 700;
-  margin-bottom: 1rem;
-    @media(max-width: 700px){
-        text-align: center;
-        padding:0
-
-    }
-`;
-
 const ContactInfo = styled.p`
   margin: 0;
 `;
@@ -62,10 +50,8 @@ export default function Contact() {
     return (
         <>
             <ContactFooter id='contact'>
-                <ContactTitle>Contact</ContactTitle>
                 <WrapperInfo>
                     <DarkerColorContainer>Front-End Developer</DarkerColorContainer>
-                    <ContactInfo><DarkerColorContainer>Address:</DarkerColorContainer> Geo Milev, Sofia, Bulgaria</ContactInfo>
                     <ContactInfo><DarkerColorContainer>Email: </DarkerColorContainer>yordalina.metodieva@gmail.com</ContactInfo>
                     <ContactInfo><DarkerColorContainer>Phone: </DarkerColorContainer> +359 88 777 8321</ContactInfo>
                 </WrapperInfo>
